76a91887ce0816b3d0d8f4dc818701ffe3ef0f27
Gowtham <Gowthamveerendra@Gowthams-MacBook-Air.local>
2025-12-31 01:12:08 +0530

    Setup GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,22 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import "./index.css"
-import App from "./App.jsx"
-import { BrowserRouter } from "react-router-dom"
+// import { StrictMode } from "react"
+// import { createRoot } from "react-dom/client"
+// import "./index.css"
+// import App from "./App.jsx"
+// import { BrowserRouter } from "react-router-dom"
 
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+// createRoot(document.getElementById("root")).render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// )
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import { HashRouter as Router } from "react-router-dom"
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(
+  <Router>
     <App />
-  </BrowserRouter>
+  </Router>
 )
